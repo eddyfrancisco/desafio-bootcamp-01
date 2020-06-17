54ab1ca3c1e98a5eff2f72d837d5a80b35575044
Eddy Francisco <esilva.prestador@smartbpo.io>
2020-06-17 00:17:06 -0300

    First Commit

---
+++ src/app.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+
+  const { title, url, techs } = request.body;
+
+  const project = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(project);
+
+  return response.json(project);
+
+});
+
+app.put("/repositories/:id", (request, response) => {
+
+  const { id } = request.params;
+  const { title, url, techs} = request.body;
+
+  const repositoriesIndex = repositories.findIndex(project => project.id === id);
+
+  if (repositoriesIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' });
+  };
+
+  const  { likes }  = repositories[repositoriesIndex];
+
+  const project = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  };
+
+  repositories[repositoriesIndex] = project;
+
+  return response.json(project);
+
+});
+
+app.delete("/repositories/:id", (request, response) => {
+
+  const { id } = request.params;
+
+  const repositoriesIndex = repositories.findIndex(project => project.id === id);
+
+  if (repositoriesIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' });
+  };
+
+  repositories.splice(repositories, 1);
+
+  return response.status(204).send();
+
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+
+  const { id } = request.params;
+
+  const repositoriesIndex = repositories.findIndex(project => project.id === id);
+
+  let { title, url,techs, likes } = repositories[repositoriesIndex];
+
+  likes++;
+
+  let project = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  };
+
+  repositories[repositoriesIndex] = project;
+
+
+  return response.json(project);
+
+});
+
+module.exports = app;
